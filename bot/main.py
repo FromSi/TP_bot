52b781591124075b89ae24965fa5b222d2e92494
FromSi <fromsi665@gmail.com>
2019-08-04 17:41:27 +0600

    Added Flask

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,13 @@
 from flask import Flask
 
 
+app = Flask(__name__)
+
+
+@app.route("/")
 def main():
-    print('Hello world!')
-
-if __name__ == '__main__':
-    main()
+    return "Welcome!"
 
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-    # return '<h1>Test</h1>'
-
-# if __name__ == '__main__':
-    # app.run()
+if __name__ == "__main__":
+    app.run()
