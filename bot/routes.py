8eee53eafa28c0a2020bb6f35fc34f896e78dc15
FromSi <fromsi665@gmail.com>
2019-08-24 22:44:15 +0600

    Refactoring code

--- bot/routes.py
+++ bot/routes.py
@@ -2,19 +2,21 @@ import os
 from bot import app
 from bot import commands
 from bot.cron import search
-from bot.util import http_methods
 
 
 @app.route("/", methods=["GET", "POST"])
 def main():
-    return http_methods.base(
-        get='Бот для телеграм чата.',
-        post=commands.run
-    )
+    if request.method == 'GET':
+        return 'Бот для телеграм чата.'
+    elif request.method == 'POST':
+        commands.run()
+
+        return 'OK'
 
 
 @app.route(f"/{os.environ['PATH_CRON_SEARCH']}/", methods=['POST'])
 def cron_search():
-    return http_methods.base(
-        post=search.main
-    )
+    if request.method == 'POST':
+        search.main()
+
+        return 'OK'
