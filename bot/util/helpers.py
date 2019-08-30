fe70dc3ccd716e2a43d25f0663585ffad3d1cf1e
FromSi <fromsi665@gmail.com>
2019-08-30 16:48:14 +0600

    Rrrref

--- bot/util/helpers.py
+++ bot/util/helpers.py
@@ -5,7 +5,7 @@ def horoscope(user):
     horoscope_model = models.Horoscope.query.filter_by(id=user.horoscope_id).first()
     horoscope_emoji = models.Emoji.query.filter_by(id=horoscope_model.emoji_id).first()
 
-    return {'name': horoscope_model.name, 'symbol': horoscope_emoji.symbol}
+    return {'name': horoscope_model.name, 'symbol': horoscope_model.emoji_id}
 
 
 def weather(user):
