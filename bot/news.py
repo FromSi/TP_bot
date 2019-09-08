dfcf60e53ce578d3f62da85b111f003fe46366ee
FromSi <fromsi665@gmail.com>
2019-09-08 23:29:10 +0600

    Test

--- bot/news.py
+++ bot/news.py
@@ -1,14 +1,25 @@
+import re
 from bot.util.telegram import parser, api
 
 
 def listener_content():
     if parser.text() is not None:
-        pass
+        links = re.findall(
+            'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+             parser.text()
+        )
+
+        if len(links) !== 0:
+            _send_message()
     elif parser.forward_from_message_id is not None:
-        api.forwardMessage(api.CHAT_NEWS_ID, api.CHAT_ID, parser.message_id())
-        api.deleteMessage(parser.message_id, api.CHAT_ID)
+        _send_message()
     print(parser.text() is not None, parser.forward_from_message_id is not None)
 
 
 def listener_news():
     pass
+
+
+def _send_message():
+    api.forwardMessage(api.CHAT_NEWS_ID, api.CHAT_ID, parser.message_id())
+    api.deleteMessage(parser.message_id, api.CHAT_ID)
