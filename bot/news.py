3a360b5599c1bb0eca84f8e0455b12e1b31a0271
FromSi <fromsi665@gmail.com>
2019-09-08 23:17:05 +0600

    Test

--- bot/news.py
+++ bot/news.py
@@ -5,8 +5,7 @@ def listener_content():
     if parser.text() is not None:
         print(1231312313123123123123213131231312321312321311)
     elif parser.forward_from_message_id is not None:
-        print(api.CHAT_ID,api.CHAT_NEWS_ID,parser.message_id())
-        api.forwardMessage(api.CHAT_ID, api.CHAT_NEWS_ID, parser.message_id())
+        api.forwardMessage(api.CHAT_NEWS_ID, api.CHAT_ID, parser.message_id())
         api.deleteMessage(parser.message_id, api.CHAT_ID)
     print(parser.text() is not None, parser.forward_from_message_id is not None)
 
